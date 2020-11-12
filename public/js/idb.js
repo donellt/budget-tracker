b9093304d60d09ac0cd8fa2d7494c9f8e7e79ce4
Donell Work <donelltorres@yahoo.com>
2020-11-12 11:12:31 -0500

    indexed db file done

--- public/js/idb.js
+++ public/js/idb.js
@@ -1,3 +1,6 @@
+const { response } = require("express");
+const { get } = require("mongoose");
+
 // create a variable to hold db connection
 let db;
 
@@ -23,7 +26,59 @@ request.onsuccess = function (target) {
   }
 };
 
-request.onerror = function(event) {
+//error log
+request.onerror = function (event) {
   console.log("Uh oh!" + event.target.errorCode);
 };
 
+//function will be executed if attempting to submit transaction with no connection
+function saveRecord(record) {
+  //open a new transaction with the database with read and write permissions
+  const transaction = db.transaction(['pending'], 'readwrite');
+  //access the object store for 'pending'
+  const budgetStore = transaction.objectStore('pending');
+  //add record to the budgetStore with add method
+  budgetStore.add(record);
+}
+
+//function that handles the collection and posting of data from budgetStore to the server
+function checkDatabase() {
+  //open transation
+  const transaction = db.transaction(['pending'], 'readwrite');
+  //access budgetStore
+  const budgetStore = transaction.objectStore('pending');
+  //get all records from budgetStore and set to the variable getAll
+  const getAll = budgetStore.getAll();
+
+  //upon a successful .getAll() execution, run this function
+  getAll.onsuccess = function () {
+    if (getAll.result.length > 0) {
+      fetch('/api/transaction/bulk', {
+        method: 'POST',
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: 'application/json, text/plain, */*',
+          'Content-Type': 'application/json'
+        }
+      })
+        .then(response => response.json())
+        .then(serverResponse => {
+          if (serverResponse.message) {
+            throw new Error(serverResponse);
+          }
+          //open another transaction
+          const transaction = db.transaction(['pending'], 'readwrite');
+          const budgetStore = transaction.objectStore('pending');
+          budgetStore.clear();
+
+          alert('All saved transactions have been submitted!');
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
+  }
+};
+
+//listen for online app
+window.addEventListener('online', checkDatabase);
